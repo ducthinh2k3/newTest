2d1f1b19b05d2e85c0e0691d8e717c0a4ad9ee7e
ducthinh2k3 <ducthinh12tn137@gmail.com>
2023-04-08 00:02:30 +0700

    Hello

--- Inheritance Basic/Test.cpp
+++ Inheritance Basic/Test.cpp
@@ -3,42 +3,44 @@ using namespace std;
 
 class A
 {
-    private:
-        int xa = 1;
-    protected:
-        int ya = 2;
-    public:
-        int za =3;
-        void fa()
-        {
-            cout << xa << ya << za << endl;
-        }
-        void xuat()
-        {
-            cout << "Lop A" << endl;
-        };
+private:
+    int xa = 1;
+
+protected:
+    int ya = 2;
+
+public:
+    int za = 3;
+    void fa()
+    {
+        cout << xa << ya << za << endl;
+    }
+    void xuat()
+    {
+        cout << "Lop A" << endl;
+    };
 };
 
 class B : public A
 {
-    private:
-        int xb = -1;
-    protected:
-        int yb = -2;
-    public:
-        int zb = -3;
-        void fb()
-        {
-            cout << xb << yb << zb << endl;
-            cout << "Ben A ne: " << A::za << endl;    
-        }
-        void xuat()
-        {
-            cout << "Lop B" << endl;
-        };
+private:
+    int xb = -1;
 
+protected:
+    int yb = -2;
+
+public:
+    int zb = -3;
+    void fb()
+    {
+        cout << xb << yb << zb << endl;
+        cout << "Ben A ne: " << A::za << endl;
+    }
+    void xuat()
+    {
+        cout << "Lop B" << endl;
+    };
 };
-
 
 int main()
 {
@@ -48,6 +50,4 @@ int main()
     // // Gọi cách này được
     // cout << b.A::za;
     b.xuat();
-
-
 }
